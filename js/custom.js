b4a78cd3a3a7c1564a33f7788086dafc4125a673
Jose <osedread@gmail.com>
2016-08-22 20:34:32 +0100

    Added Jquery to #skills section, added .visibility() and .invisibility() methos to the script

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,42 @@
-//JQuery 
+// Create invisibility methods in JQuery
+(function($) {
+    $.fn.invisible = function() {
+        return this.each(function() {
+            $(this).css("visibility", "hidden");
+        });
+    };
+    $.fn.visible = function() {
+        return this.each(function() {
+            $(this).css("visibility", "visible");
+        });
+    };
+}(jQuery));
+
+
+//JQuery DOM Ready Function
 $(document).ready(function() {
 
-	jumbotronFade();
+	//Jumbotron fadeIn-fadeOut animation	
+	setInterval(function jumbotronFade() {
+	$("#jumbotron").fadeIn(4000).delay(600).fadeOut(3000); }, 800);
+
+	//Skills button show
+	$(".btn-skill").on("mouseenter", function() {
+		$(this).siblings(".hidden-skill").visible();
+		$(this).invisible();
+	});
+
+	$(".btn-skill").on("mouseleave", function() {
+		$(this).siblings(".hidden-skill").invisible();
+		$(this).visible();
+	});
 
 });
 
+
+/* If I put the .hide() method inside document ready,
+the user is able to see the jumbotron from the beginning
+if the loading time is slow */
 $("#jumbotron").hide();
+$(".hidden-skill").invisible();
 
-setInterval(
-	function jumbotronFade() {
-	$("#jumbotron").fadeIn(3000).delay(400).fadeOut(3000);
-
-}, 800);
