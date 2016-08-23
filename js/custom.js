4ebe7a84fa400d80bb280c17565dbf6430e4cdb2
Jose <osedread@gmail.com>
2016-08-23 01:52:52 +0100

    #skills section improved looks playing around with jquery. improved custom jquery function for toggle invisibility plus a faded effect using css transitions

--- js/custom.js
+++ js/custom.js
@@ -2,41 +2,55 @@
 (function($) {
     $.fn.invisible = function() {
         return this.each(function() {
-            $(this).css("visibility", "hidden");
+            
+            $(this).css({
+            	opacity: 1.0,
+            	visibility: "visible"})
+            .animate({opacity: 0});
         });
     };
     $.fn.visible = function() {
         return this.each(function() {
-            $(this).css("visibility", "visible");
+        	
+            $(this).css({
+            	opacity: 0,
+            	visibility: "visible"})
+            .animate({opacity: 1.0});
+
         });
     };
 }(jQuery));
-
-
-//JQuery DOM Ready Function
-$(document).ready(function() {
-
-	//Jumbotron fadeIn-fadeOut animation	
-	setInterval(function jumbotronFade() {
-	$("#jumbotron").fadeIn(4000).delay(600).fadeOut(3000); }, 800);
-
-	//Skills button show
-	$(".btn-skill").on("mouseenter", function() {
-		$(this).siblings(".hidden-skill").visible();
-		$(this).invisible();
-	});
-
-	$(".btn-skill").on("mouseleave", function() {
-		$(this).siblings(".hidden-skill").invisible();
-		$(this).visible();
-	});
-
-});
-
 
 /* If I put the .hide() method inside document ready,
 the user is able to see the jumbotron from the beginning
 if the loading time is slow */
 $("#jumbotron").hide();
-$(".hidden-skill").invisible();
+$("#skills p").invisible();
+
+
+//JQuery DOM Ready Function
+$(document).ready(function() {
+
+	// Smooth Scroll
+
+
+	//Jumbotron fadeIn-fadeOut animation	
+	setInterval(function jumbotronFade() {
+	$("#jumbotron").fadeIn(2000).delay(5000).fadeOut(3000); }, 800);
+
+	//Skills button show
+	$(".btn-skill").on("mouseenter", function() {
+		$(this).siblings("p").visible();
+		$(this).css("opacity", "0.3");
+	});
+
+	$(".btn-skill").on("mouseleave", function() {
+		$(this).siblings("p").delay(400).invisible();
+		$(this).css("opacity", "1");
+	});
+
+});
+
+
+
 
