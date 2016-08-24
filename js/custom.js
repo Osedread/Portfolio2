d594f5871ecacb012c8acf09d215b9bcf9a6c64f
Jose <osedread@gmail.com>
2016-08-24 13:54:55 +0100

    Changed animation in landing from Jquery to CSS. Finished Glyphicons

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-// Create invisibility methods in JQuery
+// Create fade effect methods in JQuery
 (function($) {
     $.fn.invisible = function() {
         return this.each(function() {
@@ -24,7 +24,7 @@
 /* If I put the .hide() method inside document ready,
 the user is able to see the jumbotron from the beginning
 if the loading time is slow */
-$("#jumbotron").hide();
+// $("#jumbotron").hide();
 $("#skills p").invisible();
 
 
@@ -32,12 +32,48 @@ $("#skills p").invisible();
 $(document).ready(function() {
 
 	// Smooth Scroll
-
-
+  var scrollToAnchor = function( id ) {
+ 
+    // grab the element to scroll to based on the name
+    var elem = $("a[name='"+ id +"']");
+ 
+    // if that didn't work, look for an element with our ID
+    if ( typeof( elem.offset() ) === "undefined" ) {
+      elem = $("#"+id);
+    }
+ 
+    // if the destination element exists
+    if ( typeof( elem.offset() ) !== "undefined" ) {
+ 
+      // do the scroll
+      $('html, body').animate({
+              scrollTop: elem.offset().top
+      }, 1000 );
+ 
+    }
+  };
+ 
+  // bind to click event
+  $("a").click(function( event ) {
+ 
+    // only do this if it's an anchor link
+    if ( $(this).attr("href").match("#") ) {
+ 
+      // cancel default event propagation
+      event.preventDefault();
+ 
+      // scroll to the location
+      var href = $(this).attr('href').replace('#', '');
+      scrollToAnchor( href );
+ 
+    }
+ 
+  });
+/*
 	//Jumbotron fadeIn-fadeOut animation	
 	setInterval(function jumbotronFade() {
 	$("#jumbotron").fadeIn(2000).delay(5000).fadeOut(3000); }, 800);
-
+*/
 	//Skills button show
 	$(".btn-skill").on("mouseenter", function() {
 		$(this).siblings("p").visible();
